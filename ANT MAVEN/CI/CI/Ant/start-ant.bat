9f4aba34b521e9850a1c93e51c02b5d550649000
RedHot099 <redhotdevil996@gmail.com>
2020-06-25 20:31:51 +0200

    'zadanie2'

---
+++ ANT MAVEN/CI/CI/Ant/start-ant.bat
@@ -0,0 +1,8 @@
+@echo off
+set ANT_HOME=C:\Software\apache-ant-1.10.5
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_201
+set PATH=%ANT_HOME%\bin;%PATH%
+@echo on
+@echo "You are ready to use Ant :)."
+@echo off
+cmd /k
---
+++ ANT MAVEN/CI/CI/Maven/start-maven.bat
@@ -0,0 +1,8 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_201
+set MAVEN_HOME=C:\software\apache-maven-3.6.2-bin\apache-maven-3.6.2
+set PATH=%MAVEN_HOME%\bin;%PATH%
+@echo on
+@echo "You are ready to use Maven :)."
+@echo off
+cmd /k
